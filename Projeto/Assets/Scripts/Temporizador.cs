228af7aa1260f1244f4a09191228fb95ed859265
juannunes <juannunes007@gmail.com>
2021-01-29 00:16:57 -0300

    atualizacao_Temporizador

--- Projeto/Assets/Scripts/Temporizador.cs
+++ Projeto/Assets/Scripts/Temporizador.cs
@@ -5,10 +5,13 @@ using UnityEngine.UI;
 
 public class Temporizador : MonoBehaviour
 {
-    public Text cronometro_txt;
-    private float cronometro=0f; // tempo 
+    public Text segundos_txt;
+    public Text minutos_txt;
+    private float segundos=0f;
+    private float minutos = 0f;
+    private float maximoSegundos = 60f;
     public static bool stopTime;
-    private int tempoMaximo=120;
+    private int tempoMaximo=10;
     public GameObject pausePainel;
 
     void Start()
@@ -17,14 +20,22 @@ public class Temporizador : MonoBehaviour
     }
     void Update()
     {
-        if(cronometro>=tempoMaximo)
+        if(minutos>=tempoMaximo)
         {
             stopTime = true;
         }   
         if (stopTime==false)
         {
-            cronometro += Time.deltaTime;
-            cronometro_txt.text = cronometro.ToString("F0");
+            segundos += Time.deltaTime;
+            segundos_txt.text = segundos.ToString("F0");
+            if (segundos>=maximoSegundos)
+            {
+                minutos++;
+                segundos = 0 + 1;
+                minutos_txt.text = minutos.ToString("F0");
+
+            }
+
 
         }
         else
